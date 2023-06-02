4f03b23cf18a4fcff4e6a2c89766556b06e699ec
JaaJPlayz <hiagoaugustofcunha@gmail.com>
2023-06-02 20:13:52 -0300

    Changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
+//Libraries imports
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 // Components Imports
 import Navbar from "./components/Navbar";
 
-function App() {
+//Pages imports
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Products from "./pages/Products";
+
+const App = () => {
   return (
-    <>
-      <div className="App">
-        <Navbar></Navbar>
-      </div>
-    </>
+    <Router>
+      <>
+        <div className="App">
+          <Navbar />
+          <Routes>
+            <Route path="/home" element={<Home />}></Route>
+            <Route path="/about" element={<About />}></Route>
+            <Route path="/contact" element={<Contact />}></Route>
+            <Route path="/products" element={<Products />}></Route>
+          </Routes>
+        </div>
+      </>
+    </Router>
   );
-}
+};
 
 export default App;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,6 @@
+// Libraries imports
+import { Link } from "react-router-dom";
+// Styles imports
 import styles from "./styles/navbar.module.css";
 
 const Navbar = () => {
@@ -7,6 +10,12 @@ const Navbar = () => {
         <div className={styles.siteLogo}>
           <img src="/image/bear.png" alt="A little bear" />
           <h1>Beary Store</h1>
+        </div>
+        <div className={styles.routesDiv}>
+          <Link to={"/home"}>Home</Link>
+          <Link to={"/about"}>About</Link>
+          <Link to={"/contact"}>Contact</Link>
+          <Link to={"/products"}>Products</Link>
         </div>
       </div>
     </>
